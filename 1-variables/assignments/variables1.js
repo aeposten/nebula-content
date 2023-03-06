880a0bbdd1d81c9facb1f4b8974bcb314ce4fdf9
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-06 15:23:13 -0800

    Finished variable hw

---
+++ 1-variables/assignments/variables1.js
@@ -0,0 +1,28 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const petName = "Magnus";
+const age = 2;
+const species = "canine";
+const breed = "boxer";
+const weightInLbs = 80;
+const isGoodBoy = true;
+
+const magnusString = `My pet's name is ${petName}. He is a ${species} and his breed is ${breed}. His weight is ${weightInLbs} lbs. Is he a good boy? ${isGoodBoy}`
+
+console.log(magnusString);
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const vehicleType = "boat";
+const boatType = "canoe";
+const boatMaterial = "wood";
+const boatAge = 12;
+const boatColor = "red";
+const doesBoatFloat = false;
+
+const boatString = `My vehicle is a ${vehicleType}. It is a ${boatType}. It's color is ${boatColor}. Its material is ${boatMaterial}. Does it float? ${doesBoatFloat}`;
+
+console.log(boatString);
