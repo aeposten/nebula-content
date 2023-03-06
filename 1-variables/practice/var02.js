9f4401b1e8baedf64e1edff9b5cf8ccc9dab1d57
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-06 15:41:15 -0800

    Completed variable practice two

---
+++ 1-variables/practice/var02.js
@@ -0,0 +1,30 @@
+// Create a variable for each aspect of an address 
+// First Name, Last Name, House-Number, Street-Name, City, State, Zip
+// Combine them to be usable as a postage address
+
+/*
+First-Name Last-Name
+House-Number, Street-Name 
+City, State
+Zip
+
+Your log should look like this:
+
+Steve Rogers
+569, Amsterdam Ave
+New York, NY
+18062 
+*/
+
+const fName = "Bob";
+const lName = "Jones";
+const houseNum = 72;
+const street = "Sesame";
+const city = "Santa Clara";
+const state = "California";
+const zip = 95051;
+const address = `${fName} ${lName} 
+${houseNum}, ${street}
+${city}, ${state}
+${zip}`
+
