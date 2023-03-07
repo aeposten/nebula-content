77d87dc2c6df1cebed22664a53468a12455c90de
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-07 11:28:36 -0800

    Updated spacing

--- 1-variables/practice/variable-practice-1.js
+++ 1-variables/practice/variable-practice-1.js
@@ -3,18 +3,21 @@ const myName = "Amy";
 const nameLog = `My name is ${myName}`;
 
 console.log(nameLog);
+
 // 2. Declare a new variable and set it equal to a number
 const myNum = 12;
 const numLog = `Here is a number ${myNum}`;
 
 console.log(numLog);
+
 // 3. Set a variable to a boolean value
 const myBool = false;
 
 const boolLog = `Here is a boolean: ${myBool}`;
+
 // 4. Create a variable called carName, assign the value Volvo to it.
 const carName = "Volvo";
-const carLog = `Let's log a car ${carName}`;
+const carLog = `Let's log a car: ${carName}`;
 
 console.log(carLog);
 // 5. Create a variable called num, assign the value 50 to it.
@@ -22,11 +25,13 @@ const num = 50;
 const fiftyLog = `The number is ${num}!`;
 
 console.log(fiftyLog);
+
 // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
 const num1 = 5;
 const num2 = 3;
 const sum = num1 + num2;
 
 console.log(`${num1} plus ${num2} equals ${sum}.`);
+
 // 7. Create a variable called num3, assign the addition of num1 and num2 to it.
 const num3 = sum;
---
+++ 4-control-flow/assignments/control-flow-hw.js
@@ -0,0 +1,111 @@
+//1. Write a statement that checks if 2 and 'two' are equal
+const twoWord = 'two';
+const twoNum = 2;
+const twoEquality = (twoWord === twoNum);
+
+console.log(`Are two and 2 equal? ${twoEquality}`);
+//2. Write a statement that checks if '2' and 2 are loosely equal
+const twoStr = '2';
+const twoLooseEquality = (twoStr == twoNum);
+
+console.log(`Are 2 and '2' loosely equal? ${twoLooseEquality}`);
+//3. Write a statement that checks if 14 and '14' are strictly equal
+const fourteenNum = 14;
+const foutreenStr = '14';
+const fourteenEquality = (fourteenNum === foutreenStr);
+
+console.log(`Are 14 and '14' strictly equal? ${fourteenEquality}`);
+//4. Write a statement that checks if 10 is greater than -100
+const ten = 10;
+const negNum = -100;
+const numGreater = (ten > negNum);
+
+console.log(`Is 10 greater than -100 ? ${numGreater}`)
+//5. Write a statement that checks if 4 is strictly equal to the rounded value of 4.4
+const four = 4;
+const roundedfour = Math.round(4.4);
+const equalFours = (four === roundedfour);
+
+console.log(`Is 4 strictly equal to 4.4 rounded? ${equalFours}`);
+//6. Write a statement that checks if 10 is greater than or equal to 9.1 rounded up
+const nineRounded = Math.ceil(9.1);
+const tenEquality = (ten >= nineRounded);
+
+console.log(tenEquality);
+//7. Write a statement that checks if 10 is greater than 2 and 3 is greater than 4
+  // 7.a Without changing any of the numbers, or their order. How do we make this print true?
+
+//8. Pick a number between 1-20 and assign it to a variable
+  // This is the number of watermelons you purchased.
+  //
+  // If your number of watermelons is 1 or 2, console.log 'Looks like you like watermelon'
+  // If your number of watermelons is more than 2, console.log 'Thats a lot of watermelon'
+  // If your number of watermelons is more than 5, print, 'Thats too many watermelon!'
+  // If your number of watermelons is more than 10, print, 'You must be CRAZY for watermelon!'
+  // If your number of watermelons is more than 15, print, 'Did you have a coupon?'
+
+//   if(numOfWM === 1 || numOfWM ===  2) {
+//     console.log('Looks like you like watermelon')
+//   } else if (numOfWM > 2 && numOfWM <= 5) {
+//     console.log('Thats a lot of watermelon')
+//   } else if (numOfWM > 5 && numOfWM <= 10){
+//     console.log('Thats too many watermelon!')
+//   } else if (numOfWM > 10 && numOfWM <= 15) {
+//     console.log('You must be CRAZY for watermelon!')
+//   } else {
+//     console.log('Did you have a coupon?')
+//   }
+
+// //9. Create a variable that represents your current hunger on a level of 1-10
+//   // Using a ternary, if your hunger is greater than 6 print, 'Time to eat!'
+//   // Otherwise print. "I'm not hungry"
+
+
+// 10. Think of an Magic-8 Ball
+  // Write a switch statement that works the same way. 
+  // When you run this file you should
+  // Generate a random number
+  // And get an out put from your switch statement?
+  // Ex: 
+    // I update a question variable to say "Will I win the lottery?"
+    // I run node <FILENAME> 
+    // The console prints out:
+      // Will I win the lottery? Not Likely. 
+
+
+  
+  
+  
+
+  
+      const randomNum = Math.ceil(Math.random() * 4) 
+      const question = "Will I win the lottery??"
+  
+      switch(randomNum) {
+        case 1:
+          console.log(`${question}: Not Likely`)
+          break;
+        case 2:
+          console.log(`${question}: IDK maybe`)
+          break;
+        case 3:
+          console.log(`${question}: Most likely`)
+          break;
+        case 4:
+          console.log(`${question}: For sure happening`)
+          break;
+      }
+  
+  // BONUS:
+    // On the prior example, try and have the answer print like someone is speaking to you.
+    
+    // Ex:
+      //Input:  Will I win the lottery?
+      //Output: Will you win the lottery? Not likely. <- Notice I has been programmatically changed to you
+  
+    // Ex2:
+      // Input: Will I get a new pet this year?
+      // Output: Will you get a pet this year? It is certain. 
+  
+    // Try and handle many edge cases - can we put in any question and have it spit out a gramatically correct response?
+  
