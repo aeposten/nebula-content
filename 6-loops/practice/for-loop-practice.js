8adeb66dada1fce2fefcb59fdb6e40731ca6da31
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-08 10:17:01 -0800

    Moved loop homework to correct ffile, completed for loop practice

--- 6-loops/practice/for-loop-practice.js
+++ 6-loops/practice/for-loop-practice.js
@@ -1,118 +1,66 @@
-// 1.   Declare a for loop that starts at 0, goes until 30, and increments by 1
-//      Each loop it should log to the console 'the current index is _'
-for (let i = 0; i <= 30; i++) {
-  console.log(`The current index is ${i}`);
-}
-
-// 2.   Declare a for loop that starts at 1, goes until 10, and increments by 2
+// 1.   Declare a for loop that starts at 0, goes until 20, and increments by 1
 //      Each loop it should log to the console the current index
 
-for (let i = 1; i <= 10; i += 2) {
-  console.log(`The current index is ${i}`);
+for (let i = 0; i <= 20; i++) {
+  console.log(i);
 }
 
-// 3.   Create an array with your top 5 celebrity names in it.
-//      Be sure each element is a first & last name, ex: ['Martha Stewart', 'David Beckham', ...]
-//      Loop over the array and log each celebrity's full name to the console.
-const celebrityArray = [
-  "John Malkovich",
-  "Snoop Dogg",
-  "Jon Stewart",
-  "Stephen King",
-  "Beyonce Knowles",
+// 2.   Declare a for loop that starts at 1, goes until 40, and increments by 2
+//      Each loop it should log to the console the current index
+for (let i = 1; i <= 40; i += 2) {
+  console.log(i);
+}
+
+// 3.   Create an array with all student names in it.
+//      Loop over the array and log each student's name to the console.
+//      Inside the array declare an unrequired, but useful variable for 'currentStudent'
+const nameArray = [
+  "Raekwon",
+  "Ghostface Killah",
+  "Red Man",
+  "Method Man",
+  "Masta Killa",
+  "RZA",
+  "GZA",
+  "Inspectah Deck",
+  "Cappadonna",
+  "ODB(RIP)",
 ];
 
-for (let i = 0; i < celebrityArray.length; i++) {
-  console.log(celebrityArray[i]);
+for (let i = 0; i < nameArray.length; i++) {
+  let currentStudent = nameArray[i];
+  console.log(currentStudent);
 }
 
-// 4.   Loop over your celebrity list and
-//      If a celebrity's full name has an even number of characters log it to the console
+// 4.   Use the array below for the following question
+//      Loop over the numArray & ONLY log even numbers to the console.
+//      2 Hints: there will be an IF statement within the loop & be sure to check the value, not the index
+const numArray = [5, 3, 2, 1, 10, 20, 30, 22, 33, 12, 11, 9, 6, 3];
 
-for (let i = 0; i < celebrityArray.length; i++) {
-  celebrityArray[i].length % 2 === 0 ? console.log(celebrityArray[i]) : null;
-}
-
-// 5.   Use the celebrity array for this question
-//      Create a for loop which will iterate over each element and output a new array of only first names
-const firstNames = [];
-for (let i = 0; i < celebrityArray.length; i++) {
-  const spaceIndex = celebrityArray[i].lastIndexOf(" ");
-  firstNames.push(celebrityArray[i].substring(0, spaceIndex));
-}
-console.log(firstNames);
-
-// 6.   Use the celebrity array for this question
-//      Create a for loop which will iterate over each element and output a new array of only last names
-
-const lastNames = [];
-for (let i = 0; i < celebrityArray.length; i++) {
-  const spaceIndex = celebrityArray[i].lastIndexOf(" ");
-  lastNames.push(
-    celebrityArray[i].substring(spaceIndex, celebrityArray[i].length)
-  );
-}
-console.log(lastNames);
-
-// 7.   Loop over the celebrity list and then loop through each name.
-//      If a letter is a vowel, log it to the console
-const vowels = "aeiou";
-for (let i = 0; i < celebrityArray.length; i++) {
-  for (let j = 0; j < celebrityArray[i].length; j++) {
-    if (vowels.includes(celebrityArray[i][j])) {
-      console.log(celebrityArray[i][j]);
-    }
+for (let i = 0; i < numArray.length; i++) {
+  if (numArray[i] % 2 === 0) {
+    console.log(numArray[i]);
   }
 }
 
-// 8.   Loop over the celebrity list and then loop through each name.
-//      If a letter is uppercase, log it to the console
-for (let i = 0; i < celebrityArray.length; i++) {
-  for (let j = 0; j < celebrityArray[i].length; j++) {
-    if (celebrityArray[i][j] === celebrityArray[i][j].toUpperCase()) {
-      console.log(celebrityArray[i][j]);
-    }
-  }
-}
-// 9.   Convert your celebrity array to all caps and no spaces:
-//      Ex:
-//        Input:  ['Martha Stewart', 'David Beckham', etc..]
-//        Output: ['MARTHA-STEWART', 'DAVID-BECKHAM', etc..]
-
-for (let i = 0; i < celebrityArray.length; i++) {
-  celebrityArray[i] = celebrityArray[i].toUpperCase().replace(" ", "-");
-}
-
-console.log(celebrityArray);
-// 10.  Index your array to find your favorite celebrity.
-//      Save that name to a variable
-//      Loop over the array until you find that individual
-//      Log that name to the console and break out of the loop
-const favoriteCelebrity = celebrityArray[1];
-
-for (let i = 0; i < celebrityArray.length; i++) {
-  if (celebrityArray[i] === favoriteCelebrity) {
-    console.log(celebrityArray[i]);
-    break;
-  } else {
-    console.log("nope");
+// 5.   Use the above numArray array for this question
+//      Create a for loop which will iterate over each element of the above numArray and output a new array of only odd numbers
+for (let i = 0; i < numArray.length; i++) {
+  if (numArray[i] % 2 !== 0) {
+    console.log(numArray[i]);
   }
 }
 
-//BONUS:
-//      Write a loop that iterates from zero until 30
-//      If an index is divisible by 2 log 'fizz'
-//      If an index is divisible by 3 log 'buzz'
-//      If an index is divisible by both 2 & 3, log 'fizz-buzz'
-//      Otherwise print the index to the console
-for (let i = 0; i <= 30; i++) {
-  if (i % 2 === 0 && i % 3 === 0) {
-    console.log("fizz-buzz");
-  } else if (i % 2 === 0) {
-    console.log("fizz");
-  } else if (i % 3 === 0) {
-    console.log("buzz");
-  } else {
-    console.log(i);
-  }
+// 6.   Using the nums and the two empty placeholder arrays below
+//      Create a for loop which will fill the two separate arrays, one with only positive numbers, one with only negative numbers
+
+const nums = [5, 3, 2, -1, 10, 20, 30, -22, 33, 12, 11, 9, 6, -3];
+const positiveArr = [];
+const negativeArr = [];
+
+for (let i = 0; i < nums.length; i++) {
+  (i % 2 === 0) ? positiveArr.push(nums[i]): negativeArr.push(nums[i]);
 }
+
+console.log(positiveArr);
+console.log(negativeArr);
