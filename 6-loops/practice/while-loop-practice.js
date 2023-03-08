f6b06e4e0610d71c7736b0fa92d933e88a58c6ae
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-08 09:15:30 -0800

    Completed while loop practice

---
+++ 6-loops/practice/while-loop-practice.js
@@ -0,0 +1,17 @@
+// 1.   Write a while loop that loops over a string until it finds the letter "a"
+const string = "thraxnt";
+
+let i = 0;
+
+while (string[i].toLowerCase() !== "a") {
+  i++;
+}
+
+// 2.   Write a while loop that logs to the console the 'count' until the 'count' reaches 100
+//      Inside the while loop be sure to increase count by some number
+
+let count = 0;
+
+while (count < 100) {
+  count++;
+}
