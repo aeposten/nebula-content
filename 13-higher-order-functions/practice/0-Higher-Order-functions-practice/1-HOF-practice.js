5a60e52bfde1739fde46b3de3f4d5fa3404aa955
Amy <86963506+aeposten@users.noreply.github.com>
2023-04-18 16:07:26 -0700

    Completed practice 1

--- 13-higher-order-functions/practice/0-Higher-Order-functions-practice/1-HOF-practice.js
+++ 13-higher-order-functions/practice/0-Higher-Order-functions-practice/1-HOF-practice.js
@@ -6,6 +6,14 @@ Write the callback functions separately
     calculate(subtract, 2,4) => -2
     calculate(exponent, 2,4) => 16   */
 
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function calculate(operator, num1, num2) {
+  return operator(num1, num2);
+}
+
 /* Question 2
 Write a function that takes in any number of numbers and performs calculations on them
 console.log(arguments) inside of a function to visualize your arguments
@@ -15,6 +23,14 @@ Write the callback functions separately
     calculateAll(subtract, 2,4,5,7) => -14
 */
 
+function calculateAll(operator, arr) {
+  let total = 1;
+  for (let i = 0; i < arr.lenght; i++) {
+    total = operator(total, arr[i]);
+  }
+  return total;
+}
+
 /* Question 3
 Write a function that takes in a string and performs some form of manipulation on it
   Example:
@@ -22,6 +38,45 @@ Write a function that takes in a string and performs some form of manipulation o
     modify(sarcastic, 'I really like running') => I ReAlLy lIkE RuNnIng
     modify(code, 'There is no cake') => Th-r- -s n- c-k-
 */
+
+function yeller(string) {
+  return string.toUpperCase();
+}
+
+function sarcastic(string) {
+  let newStr = "";
+
+  for (let i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      newStr += string[i].toUpperCase();
+    } else {
+      newStr += string[i].toLowerCase();
+    }
+  }
+  return newStr;
+}
+
+function code(string) {
+  const vowels = "aeiou"
+  let newStr = "";
+
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i])) {
+      newStr += "-";
+    } else {
+      newStr += string[i];
+    }
+  }
+  return newStr;
+} 
+
+function modify(manipulation, string) {
+  return manipulation(string);
+}
+
+// console.log(modify(yeller, "I need a nap"));
+// console.log(modify(sarcastic, "I really like running"));
+// console.log(modify(code, 'There is no cake'))
 
 /* Question 4
 Write a function that takes in an array filled with different data types and returns each data type
@@ -38,5 +93,25 @@ Write a function that takes in an array filled with different data types and ret
     =>
     [10, Infinity]
 */
-    
-    
+
+function checkType(item) {
+  let type = null;
+  if (Array.isArray(item)) {
+    type = "array";
+  } else {
+    type = typeof item
+  } return type
+}
+
+function oneType(type, arr) {
+  let typeArray = [];
+
+  for (let i=0; i < arr.length; i++) {
+    if (type === checkType(arr[i])) {
+      typeArray.push(arr[i])
+    }
+  }
+  return typeArray
+}
+
+console.log(oneType("string", ['I love coding', 10, undefined, Infinity, 'goosfraba', null, ['Nested Array!']]))
