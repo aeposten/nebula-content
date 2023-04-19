4670cbed11498642a11e4d1b23a1a1688ac35286
Amy <86963506+aeposten@users.noreply.github.com>
2023-04-19 09:16:54 -0700

    Completed hof practice 3

--- 13-higher-order-functions/practice/0-Higher-Order-functions-practice/3-HOF-practice.js
+++ 13-higher-order-functions/practice/0-Higher-Order-functions-practice/3-HOF-practice.js
@@ -4,18 +4,36 @@
 Using .map() create a new array that doubles all the numbers in a given array */
 const numbers = [ 1, 2, 3, 4, 5 ]
 
+const doubled = numbers.map((number) => number * 2);
+
+// console.log(doubled);
+
 /* Question 2
 Using .map() create a new array that halves all numbers in a given array */
+const halved = numbers.map((number) => number / 2);
+
+// console.log(halved);
 
 /* Question 3
 Using .map() create a new array that squares all of the numbers in a given array */
+const squared = numbers.map((number) => number ** 2);
+
+// console.log(squared);
 
 /* Question 4
 Using .map() create a new array of all capital strings from an array of lower case strings */
 const lowerCase = ['hello', 'world', 'how', 'are', 'you']
+
+const upperCase = lowerCase.map((word) => word.toUpperCase());
+
+// console.log(upperCase);
 
 /* Question 5
 Using .map() create a new array of greeting sentences from an array of given people
   Expected output:
     ['Hello Pamela', 'Hello Michael', 'Hello Angela'] */
 const people = ['Pamela', 'Michael', 'Angela']
+
+const helloPeople = people.map((person) => `Hello ${person}`)
+
+console.log(helloPeople);
