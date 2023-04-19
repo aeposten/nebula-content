14b9b954f16ab452a8bad7f25b6ec8f93d844839
Amy <86963506+aeposten@users.noreply.github.com>
2023-04-19 11:55:26 -0700

    Completed HoF practice 7

--- 13-higher-order-functions/practice/0-Higher-Order-functions-practice/7-HOF-practice.js
+++ 13-higher-order-functions/practice/0-Higher-Order-functions-practice/7-HOF-practice.js
@@ -4,10 +4,22 @@
 Write a HOF to find the first number greater than 10 in an array */
 const nums = [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 ]
 
+const greaterThan10 = nums.find(num => num > 10);
+
+// console.log(greaterThan10);
+
 /* Question 2
 Write a HOF to find the first string in an array */
 const arr = [1,2,3,"pizza", "tacos","pie", {}, [], null, 123]
 
+const string = arr.find((item) => typeof item === "string");
+
+// console.log(string);
+
 /* Question 3
 Write a HOF to find the first instance of null in an array */
 const arr2 = [1,2,3,"pizza", "tacos","pie", {}, [], null, 123]
+
+const isNull = arr.find((item) => item === null);
+
+console.log(isNull)
