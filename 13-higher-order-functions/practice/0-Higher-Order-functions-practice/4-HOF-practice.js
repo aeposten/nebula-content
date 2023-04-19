7c3a038b1ab68e589df184d968462b5740657c26
Amy <86963506+aeposten@users.noreply.github.com>
2023-04-19 11:04:14 -0700

    Completed HoF practice 4

--- 13-higher-order-functions/practice/0-Higher-Order-functions-practice/4-HOF-practice.js
+++ 13-higher-order-functions/practice/0-Higher-Order-functions-practice/4-HOF-practice.js
@@ -8,12 +8,40 @@ Given an array of people, log a greeting to the console for each person. */
 
 /* Question 3
 Given an array of mixed values, log whether or not each value is undefined. */
-const mixed = [1, 'hello', undefined, true, null, false, 'world', undefined]
+const mixed = [1, "hello", undefined, true, null, false, "world", undefined];
+// mixed.forEach((item => console.log(item === undefined)))
 
 /* Question 4
 Given an array of strings, log the vowel count for each string. */
-const arrayOfStrings = ['Hi!', '🎶', "It's", 'a', 'beautiful', 'day', 'in', 'the', 'neighborhood', '🎶']
+const arrayOfStrings = [
+  "Hi!",
+  "🎶",
+  "It's",
+  "a",
+  "beautiful",
+  "day",
+  "in",
+  "the",
+  "neighborhood",
+  "🎶",
+];
+
+function logVowelCount(word) {
+  const vowels = "aeiou";
+  word = word.toLowerCase();
+  let count = 0;
+  for (letter in word) {
+    if (vowels.includes(word[letter])) {
+      count++;
+    }
+  }
+  console.log(count);
+}
+
+// arrayOfStrings.forEach((string) => logVowelCount(string));
 
 /* Question 5
 Given an array of numbers, double each of them and log them to the console */
-const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number) => console.log(number * 2))
